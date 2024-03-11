27c9b3eaffbe956f33c3dc7eee07345bd414a933
JonathanT11 <151862669+JonathanT11@users.noreply.github.com>
2024-03-11 13:55:26 -0700

    Add files via upload

---
+++ timers2.js
@@ -0,0 +1,30 @@
+function countDown (time) {
+ let timer = setInterval(function(){
+    time--; 
+    if(time <= 0){ 
+        clearInterval(timer);
+    console.log(`DONE!!!`);
+}
+else { 
+    console.log(time);
+}
+ } ,500)
+ 
+}
+
+
+
+function randomGame(){
+    let num;
+    let times = 0;
+    let timer = setInterval (function(){
+        num = Math.random();
+        times++
+        if (num > .75) {
+            clearInterval(timer);
+            console.log(`It took ` + times + ` tries .`);
+        }
+    },1)
+}
+
+
